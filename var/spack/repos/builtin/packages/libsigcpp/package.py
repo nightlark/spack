ec956df4e3b4136b63543efc83de36e8782ea211
Ryan Mast <mast9@llnl.gov>
2020-12-14 20:36:01 -0800

    libsigcpp: update latest stable version 2.10.6

--- var/spack/repos/builtin/packages/libsigcpp/package.py
+++ var/spack/repos/builtin/packages/libsigcpp/package.py
@@ -15,6 +15,7 @@ class Libsigcpp(AutotoolsPackage):
     list_depth = 1
 
     version('2.99.12', sha256='d902ae277f5baf2d56025586e2153cc2f158472e382723c67f49049f7c6690a8')
+    version('2.10.6', sha256='dda176dc4681bda9d5a2ac1bc55273bdd381662b7a6d49e918267d13e8774e1b')
     version('2.9.3', sha256='0bf9b301ad6198c550986c51150a646df198e8d1d235270c16486b0dda30097f')
     version('2.1.1', sha256='7a2bd0b521544b31051c476205a0e74ace53771ec1a939bfec3c297b50c9fd78')
     version('2.0.3', sha256='6ee6d5f164d8a34da33d2251cdb348b4f5769bf993ed8a6d4055bd47562f94a2')
